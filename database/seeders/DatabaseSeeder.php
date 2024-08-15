87525c3001c593fffe468370bfb2d6dbac084a59
apolo-17 <apolinar.morales.a@gmail.com>
2024-08-14 21:48:10 -0600

    Create migration tables and data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Recipe;
+use App\Models\Tag;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,7 +20,17 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'apolo@test.com'
         ]);
+        Category::factory(12)->create();
+        Recipe::factory(100)->create();
+        Tag::factory(100)->create();
+
+        $recipes = Recipe::all();
+        $tags = Tag::all();
+
+        foreach ($recipes as $recipe) {
+            $recipe->tags()->attach($tags->random(rand(2, 4)));
+        }
     }
 }
