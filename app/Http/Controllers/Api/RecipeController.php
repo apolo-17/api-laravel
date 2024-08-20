59af21ab64551b07f4db8c26849d59006bd46c1c
apolo-17 <apolinar.morales.a@gmail.com>
2024-08-19 18:18:45 -0600

    Add custom request to create and update recipes

--- app/Http/Controllers/Api/RecipeController.php
+++ app/Http/Controllers/Api/RecipeController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreRecipeRequest;
+use App\Http\Requests\UpdateRecipeRequest;
 use App\Http\Resources\RecipeResource;
 use App\Models\Recipe;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class RecipeController extends Controller
 {
@@ -14,23 +17,32 @@ class RecipeController extends Controller
         return RecipeResource::collection(Recipe::with('category', 'tags', 'user')->get());
     }
 
+    public function store(StoreRecipeRequest $request)
+    {
+        $recipe = Recipe::create($request->all());
+        if ($tags = json_decode($request->tags)) {
+            $recipe->tags()->attach($tags);
+        }
+        return response()->json(new RecipeResource($recipe->load('category', 'tags', 'user')), Response::HTTP_CREATED);
+    }
+
     public function show(Recipe $recipe)
     {
         return new RecipeResource($recipe->load('category', 'tags', 'user'));
     }
 
-    public function store(Request $request)
+    public function update(UpdateRecipeRequest $request, Recipe $recipe)
     {
-        //return $recipe->load('categories', 'tags', 'user');
-    }
-
-    public function update(Recipe $recipe)
-    {
-        //return $recipe->load('categories', 'tags', 'user');
+        $recipe->update($request->all());
+        if ($tags = json_decode($request->tags)) {
+            $recipe->tags()->sync($tags);
+        }
+        return response()->json(new RecipeResource($recipe->load('category', 'tags', 'user')), Response::HTTP_OK);
     }
 
     public function destroy(Recipe $recipe)
     {
-        //return $recipe->load('categories', 'tags', 'user');
+        $recipe->delete();
+        return response()->json(null, Response::HTTP_NO_CONTENT); //204
     }
 }
